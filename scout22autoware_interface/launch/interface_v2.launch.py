3724c630c73b798eca7ca28d0d9215db104f445f
Hikigaya-Yukina <cqhyq159@163.com>
2024-11-11 15:26:19 +0800

    更改lanuch文件的包名称

--- scout22autoware_interface/launch/interface_v2.launch.py
+++ scout22autoware_interface/launch/interface_v2.launch.py
@@ -4,12 +4,12 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='interface_v2',
+            package='scout22autoware_interface',
             executable='AUTO2SCOUT',
             name='Autoscoutpub'
         ),
         Node(
-            package='interface_v2',
+            package='scout22autoware_interface',
             executable='SCOUT2AUTO',
             name='Autoscoutsub'
         ),
