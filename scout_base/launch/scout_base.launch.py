17ee6f0402d152062d9ec6835a5bf901405c4511
Hikigaya-Yukina <87561281+Hikigaya-Yukina@users.noreply.github.com>
2024-12-19 16:28:20 +0800

    Update scout_base.launch.py

--- scout_base/launch/scout_base.launch.py
+++ scout_base/launch/scout_base.launch.py
@@ -15,11 +15,11 @@ def generate_launch_description():
 
     port_name_arg = DeclareLaunchArgument('port_name', default_value='can0',
                                          description='CAN bus name, e.g. can0')
-    odom_frame_arg = DeclareLaunchArgument('odom_frame', default_value='odom',
+    odom_frame_arg = DeclareLaunchArgument('odom_frame', default_value='scout_odom',
                                            description='Odometry frame id')
-    base_link_frame_arg = DeclareLaunchArgument('base_frame', default_value='base_link',
+    base_link_frame_arg = DeclareLaunchArgument('base_frame', default_value='scout_base_link',
                                                 description='Base link frame id')
-    odom_topic_arg = DeclareLaunchArgument('odom_topic_name', default_value='odom',
+    odom_topic_arg = DeclareLaunchArgument('odom_topic_name', default_value='scout_odom',
                                            description='Odometry topic name')
 
     is_scout_mini_arg = DeclareLaunchArgument('is_scout_mini', default_value='false',
